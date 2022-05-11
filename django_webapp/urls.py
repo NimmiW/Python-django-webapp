4e28e06e25568f53a828e2f957acd5656bc3543e
Nimmi Weeraddana <nrweerad@v1040-wn-rt-a-31-183.campus-dynamic.uwaterloo.ca>
2022-05-11 15:58:50 -0400

    new changes

--- django_webapp/urls.py
+++ django_webapp/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+#comments
 
 urlpatterns = [
     path('admin/', admin.site.urls),
